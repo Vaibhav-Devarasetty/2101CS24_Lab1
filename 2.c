2b2341f72dad4114f9c5ace12a65fd6b44e0c78b
Devarasetty Sri Vaibhav <110110877+Vaibhav-Devarasetty@users.noreply.github.com>
2022-07-27 23:54:19 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -46,6 +46,21 @@ void sort_by_selection_and_print(int array[], int n){
     printf("\n");
 }
 
+void bubbleSort_and_print(int arr[], int n){
+    int j;
+    for (int i = 0; i < n - 1; i++){
+
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }    
+    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 
 
 int main()
