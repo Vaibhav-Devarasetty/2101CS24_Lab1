05f067016f610a3a14de25b51dfa9a1c64f89ece
Devarasetty Sri Vaibhav <110110877+Vaibhav-Devarasetty@users.noreply.github.com>
2022-07-27 22:02:04 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -19,6 +19,9 @@ int main() {
         case '*':
           printf("%.1lf * %.1lf = %.1lf", x, y, x * y);
           break;
+        case '/':
+          printf("%.1lf / %.1lf = %.1lf", x, y, x / y);
+          break;
     }
     
     return 0;
