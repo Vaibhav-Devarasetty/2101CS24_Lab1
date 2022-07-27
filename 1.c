826206d3d1f11c23ac0fb82a5b136db312c5e1c3
Devarasetty Sri Vaibhav <110110877+Vaibhav-Devarasetty@users.noreply.github.com>
2022-07-27 21:59:21 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,5 +4,10 @@ int main() {
     
     char operation;
     double x, y;
+    printf("Enter an operator (+, -, *, /) : ");
+    scanf("%c", &operation);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &x, &y);
+    
     return 0;
 }
