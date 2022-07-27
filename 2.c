c42c76dc77899e0c79fc8f499aee4f07ea4c854f
Devarasetty Sri Vaibhav <110110877+Vaibhav-Devarasetty@users.noreply.github.com>
2022-07-27 23:48:54 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+void sort_by_insertion_and_print(int arr[], int n){
+    int p, slider, q;
+    for (p = 1; p < n; p++){
+        slider = arr[p];
+        q = p - 1;
+
+        while (q >= 0 && arr[q] > slider){
+            arr[q + 1] = arr[q];
+            q = q - 1;
+        }
+        arr[q + 1] = slider;
+    }
+
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+        printf("\n");
+    }
+}
+
+
 int main()
 {
     int n;
